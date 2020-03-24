30b66df5b72565118dc326c76c5bc6f698c1e3c0
Daniel Chell <dgchell@gmail.com>
2020-03-24 19:51:56 -0400

    made change to webpack build and dev modes

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-module.exports = {
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = (env) => ({
   entry: {
-    login: path.resolve(__dirname, "./client/login.jsx"),
-    second: path.resolve(__dirname, "./client/second.jsx")
+    login: path.resolve(__dirname, './client/login.jsx'),
+    second: path.resolve(__dirname, './client/second.jsx'),
   },
   output: {
-    path: path.resolve(__dirname, "./dist")
+    path: path.resolve(__dirname, './dist'),
   },
-  mode: "development",
+  mode: env.NODE_ENV,
   devServer: {
     port: 8080,
-    publicPath: "/dist/",
+    publicPath: '/dist/',
     proxy: {
-      "/": {
-        target: "http://localhost:3000"
-      }
-    }
+      '/': {
+        target: 'http://localhost:3000',
+      },
+    },
   },
   module: {
     rules: [
@@ -24,24 +24,24 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"]
-          }
-        }
-      }
-    ]
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      chunks: ["login"],
-      template: path.resolve(__dirname, "./client/login.html"),
-      filename: "login.html"
+      chunks: ['login'],
+      template: path.resolve(__dirname, './client/login.html'),
+      filename: 'login.html',
     }),
     new HtmlWebpackPlugin({
-      chunks: ["second"],
-      template: path.resolve(__dirname, "./client/secondpage.html"),
-      filename: "secondpage.html"
-    })
-  ]
-};
+      chunks: ['second'],
+      template: path.resolve(__dirname, './client/secondpage.html'),
+      filename: 'secondpage.html',
+    }),
+  ],
+});
